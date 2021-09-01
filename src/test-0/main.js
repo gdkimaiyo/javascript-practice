f87ed3cd05aff7f9979fd0b5b971902cd55c61c2
gdkimaiyo <gdkimaiyo@gmail.com>
2021-09-01 11:29:05 +0300

    add menu links to other practice exercises

--- src/test-0/main.js
+++ src/test-0/main.js
@@ -74,15 +74,96 @@ for (let i = 0; i < btn.length; i++) {
   });
 }
 
+// Find Duplicates
+let all_list = [
+  {
+    name: "Jane Doe",
+    houseNo: 123,
+    email: "jane.doe@user.com ",
+  },
+  {
+    name: "John Doe",
+    houseNo: 122,
+    email: "john.doe@user.com",
+  },
+  {
+    name: "Alice",
+    houseNo: 121,
+    email: "alice@user.com",
+  },
+  {
+    name: "Bob",
+    houseNo: 120,
+    email: "bob@user.com",
+  },
+  {
+    name: "Jane Doe",
+    houseNo: 123,
+    email: "jane.doe@user.com",
+  },
+  {
+    name: "Jane Doe",
+    houseNo: 124,
+    email: "jane.doe@user.com",
+  },
+]
+
+findDuplicates(all_list);
+
+// Original List => Duplicates Flagged: Same House no and email
+console.log("___ Duplicates flagged in list ___\n", all_list);
+
+let tbody = document.getElementById('tbody');
+// Populate tbody with content
+all_list.forEach((item, i) => {
+  let tr = document.createElement('tr');
+  let name = document.createElement('td');
+  let email = document.createElement('td');
+  let houseNo = document.createElement('td');
+  let duplicate = document.createElement('td');
+
+  name.textContent = item.name;
+  email.textContent = item.email;
+  houseNo.textContent = item.houseNo;
+  duplicate.textContent = (item.duplicate) ? "True" : "False";
+
+  tr.appendChild(name);
+  tr.appendChild(email);
+  tr.appendChild(houseNo);
+  tr.appendChild(duplicate);
+  tbody.appendChild(tr);
+});
+
 // HELPER Functions
 function showAllShops(data) {
   return data.filter((element) => element.country);
 }
+
 function getShops(data, code) {
   return data.filter((element) => element.country === code);
 }
+
 function removeAllChildNodes(parent) {
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
   }
+}
+
+function findDuplicates(data) {
+  const lookup = data.reduce((a, e) => {
+    a[e.email.trim()] = ++a[e.email.trim()] || 0;
+    a[e.houseNo] = ++a[e.houseNo] || 0;
+    return a;
+  }, {});
+
+  data.forEach((element, i) => {
+    if (lookup[element.email.trim()] && lookup[element.houseNo]) {
+      element['duplicate'] = true;
+    }
+  });
+  // Return all list with duplicates flagged
+  // return data;
+
+  // Return duplicates list
+  // return data.filter((element) => lookup[element.email] && lookup[element.houseNo]);
 }
