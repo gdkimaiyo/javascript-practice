504192587f2d802bc902cb4f12ae1cf73d564b32
gdkimaiyo <gdkimaiyo@gmail.com>
2021-09-01 08:32:27 +0300

    algorithm: find average score from a set of tests and their scores

--- src/test-1/main.js
+++ src/test-1/main.js
@@ -74,6 +74,63 @@ console.log("Min: ", min);
 let map_arr = arr.map((elm, i) => {return (elm + 2);});
 console.log(map_arr);
 
+// Algorithm Example: Test
+// Find average score based on tests and their score
+
+let testsA = ['test1a','test2','test1b','test1c','test3'];
+let results = ['Wrong Answer', 'Ok', 'Ok', 'runtime', 'Ok'];
+
+let myScore = findScore(testsA, results);
+console.log(myScore);
+
+function findScore(testArr, resArr) {
+  let av_score = 0;
+
+  let totalTests = 0;
+  let totalPassed = 0;
+
+  for (let i = 0; i < resArr.length; i++) {
+    let passed = false;
+    for (let j = 0; j < testArr.length; j++) {
+      if (testArr[j].indexOf(i) !== -1) {
+        if (resArr[j].toLowerCase() == "ok") {
+          passed = true;
+        } else {
+          passed = false;
+        }
+      }
+    }
+    if (passed) {
+      totalPassed += 1;
+    }
+    passed = false;
+  }
+
+  for (let i = 0; i < resArr.length; i++) {
+    let newTest = false;
+    for (let j = 0; j < testArr.length; j++) {
+      if (testArr[j].indexOf(i) !== -1) {
+        newTest = true;
+      }
+    }
+    if (newTest) {
+      totalTests += 1;
+    }
+  }
+
+  console.log("Passed", totalPassed);
+  console.log("Total Tests", totalTests);
+
+  if (totalPassed == 0) {
+    return av_score;
+  }
+
+  av_score = (totalPassed * 100) / totalTests;
+
+  return Math.round(av_score);
+}
+// end Algorithm Example: Test
+
 // END of PRACTICE
 
 try {
