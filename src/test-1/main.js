cf13b2d027ac3d0cf2d4996ab549e62cc2229ab0
gdkimaiyo <gdkimaiyo@gmail.com>
2021-09-05 23:44:51 +0300

    add main menu to js-practice index page

--- src/test-1/main.js
+++ src/test-1/main.js
@@ -1,25 +1,32 @@
 // PRACTICE
 
+console.log("SEARCH A STRING FOR A 'STRING'");
 let str = "Swahili";
-// let substr = "hili";
+console.log("String: ", str);
+console.log("String to search: ", 'swa');
+
 if (str.toLowerCase().indexOf("swa") !== -1) {
   console.log("Yes... it contains...");
 } else {
   console.log("No... does not contain...");
 }
+console.log("\n\n");
 
 // remove spaces at the end of a string
+console.log("REMOVE SPACES AT START & END OF A STRING");
 let strStr = " Welcome Home ";
-console.log(strStr, strStr.length);
+console.log("Original String, Length\n", strStr, strStr.length);
 let new_strStr = strStr.trim();
-console.log(new_strStr, new_strStr.length);
+console.log("New String, Length\n", new_strStr, new_strStr.length);
+console.log("\n\n");
 
 // Find list of duplicates
+console.log("FIND DUPLLICATES");
 const values = [
-  {id: 10, name: 'someName1'},
   {id: 10, name: 'someName1'},
   {id: 10, name: 'someName2'},
   {id: 11, name: 'someName3'},
+  {id: 10, name: 'someName1'},
   {id: 12, name: 'someName4'}
 ];
 
@@ -28,9 +35,14 @@ const lookup = values.reduce((a, e) => {
   a[e.name] = ++a[e.name] || 0;
   return a;
 }, {});
-
+console.log("Original Object");
+console.log(values);
+console.log("Duplicates");
 console.log(values.filter((e) => lookup[e.id] && lookup[e.name]));
+console.log("\n\n");
 
+// Find unique records in an object
+console.log("FIND UNIQUE RECORDS");
 const med = [
   {name: 'name1', position: 'left'},
   {name: 'name2', position: 'right'},
@@ -48,100 +60,39 @@ med.reduce((acc, curr) => {
   }
   return acc;
 }, [])
+console.log("Original list");
+console.log(med);
+console.log("Unique list");
+console.log(unique_arr);
+console.log("\n\n");
 
-console.log(unique_arr)
 
-// console.log(Object.values(blacklist));
-
+// SORT ARRAY IN ASC ORDER
+console.log("SORT ARRAY IN ASC ORDER");
 let arr = [12, 13, 1, 6, 72];
+console.log("Original Array");
 console.log(arr);
 
 //sort array
 arr.sort((a,b) => {return b-a;});
+console.log("Sorted Array");
 console.log(arr);
-
-// let new_arr = arr.slice(0,2);
-// console.log(new_arr);
+console.log("\n\n");
 
 // Find Max, min
+console.log("FIND MAX & MIN IN AN ARRAY");
 let max = Math.max(...arr);
 let min = Math.min(...arr);
-
+console.log("Array");
+console.log(arr);
 console.log("Max: ", max);
 console.log("Min: ", min);
+console.log("\n\n");
 
 // Map => Increase every element by 2
+console.log("INCREASE EVERY ITEM IN AN ARRAY BY 2 USING map()");
 let map_arr = arr.map((elm, i) => {return (elm + 2);});
-console.log(map_arr);
-
-// Algorithm Example: Test
-// Find average score based on tests and their score
-
-let testsA = ['test1a','test2','test1b','test1c','test3'];
-let results = ['Wrong Answer', 'Ok', 'Ok', 'runtime', 'Ok'];
-
-let myScore = findScore(testsA, results);
-console.log(myScore);
-
-function findScore(testArr, resArr) {
-  let av_score = 0;
-
-  let totalTests = 0;
-  let totalPassed = 0;
-
-  for (let i = 0; i < resArr.length; i++) {
-    let passed = false;
-    for (let j = 0; j < testArr.length; j++) {
-      if (testArr[j].indexOf(i) !== -1) {
-        if (resArr[j].toLowerCase() == "ok") {
-          passed = true;
-        } else {
-          passed = false;
-        }
-      }
-    }
-    if (passed) {
-      totalPassed += 1;
-    }
-    passed = false;
-  }
-
-  for (let i = 0; i < resArr.length; i++) {
-    let newTest = false;
-    for (let j = 0; j < testArr.length; j++) {
-      if (testArr[j].indexOf(i) !== -1) {
-        newTest = true;
-      }
-    }
-    if (newTest) {
-      totalTests += 1;
-    }
-  }
-
-  console.log("Passed", totalPassed);
-  console.log("Total Tests", totalTests);
-
-  if (totalPassed == 0) {
-    return av_score;
-  }
-
-  av_score = (totalPassed * 100) / totalTests;
-
-  return Math.round(av_score);
-}
-// end Algorithm Example: Test
+console.log("Original Array\n", arr);
+console.log("New Array\n", map_arr);
 
 // END of PRACTICE
-
-try {
-  null.f()
-} catch (e) {
-  // continue;
-  // console.log(e instanceof TypeError)  // true
-  // console.log(e.message)               // "null has no properties"
-  // console.log(e.name)                  // "TypeError"
-  // console.log(e.fileName)              // "Scratchpad/1"
-  // console.log(e.lineNumber)            // 2
-  // console.log(e.columnNumber)          // 2
-  // console.log(e.stack)                 // "@Scratchpad/2:2:3\n"
-}
