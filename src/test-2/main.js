82cd4d82fbfc18321ad454ed87b294c8ed7b53fa
gdkimaiyo <gdkimaiyo@gmail.com>
2021-11-18 12:16:57 +0300

    chore: additional random practice exercise

--- src/test-2/main.js
+++ src/test-2/main.js
@@ -1,4 +1,65 @@
 
+let answers = new Map;
+console.log(answers);
+
+answers.set({
+  age: 12,
+  name: "jane doe",
+});
+console.log(answers);
+
+const pets = ['cat', 'dog'];
+console.log(pets.includes('at'));
+let Pets = "cat";
+console.log(Pets);
+Pets = Pets.charAt(0).toUpperCase() + Pets.slice(1);
+console.log(Pets);
+
+const user = () => 'GIDEON';
+
+const sayHello = (greet = 'Hello', to = user()) => {
+  let greeting = `${greet} ${to}`;
+  console.log(greeting);
+}
+
+sayHello('Good Morning,');
+
+var str   = "my";
+var stringArray = str.split(/(\s+)/);
+
+console.log(stringArray); // ["my", " ", "car", " ", "is", " ", "red"]
+
+// Get first word of a string
+let sentence = "Speech facilitation and therapy";
+let firstWord = sentence.split(" ")[0]
+console.log(sentence);
+console.log(firstWord);
+
+// Get the starting time in a time slot with starting and ending time
+let strArr = ["12:00 - 12:30", "13:00 - 13:30", "14:00 - 14:30"];
+let map_strArr = strArr.map((elm, i) => {return (elm.split(" ")[0]);});
+console.log("Original Array\n", strArr);
+console.log("New Array\n", map_strArr);
+
+
+function updateVegetablesCollection (veggies, veggie) {
+    if (veggies.indexOf(veggie) === -1) {
+        veggies.push(veggie);
+        console.log('New veggies collection is : ' + veggies);
+    } else if (veggies.indexOf(veggie) !== -1) {
+        console.log(veggie + ' already exists in the veggies collection.');
+    }
+}
+
+var veggies = ['potato', 'tomato', 'chillies', 'green-pepper'];
+
+updateVegetablesCollection(veggies, 'spinach');
+// New veggies collection is : potato,tomato,chillies,green-pepper,spinach
+updateVegetablesCollection(veggies, 'spinach xy');
+// spinach already exists in the veggies collection.
+
+
+
 console.log("ALGORITHM.\nFind least stops in a machine");
 let my_str = ['712','246', '356', '312'];
 let leastStops = 0;
